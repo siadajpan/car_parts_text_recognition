b178556415743327f01376824ebf0d69eb4e883f
Karol Misiarz <karol.misiarz@cervirobotics.com>
2020-02-27 16:06:57 +0100

    Add text position recognition that uses contour analyzer

---
+++ car_parts_text_recognition/recognition/text_position_recognition_contours.py
@@ -0,0 +1,104 @@
+from typing import Optional, List, Tuple
+
+import cv2
+import numpy as np
+
+from car_parts_text_recognition.image_processor.image_processor import \
+    ImageProcessor
+from car_parts_text_recognition.recognition.bound_box_analyzer import \
+    BoundBoxAnalyzer
+from car_parts_text_recognition.recognition.utils.bound_box import BoundBox
+
+
+class TextPositionRecognitionContours:
+    """
+    Recognition of text on the image with the use of contour analyzing.
+    """
+    def __init__(self):
+        self._image_processor = ImageProcessor()
+        self._image: Optional[np.array] = None
+        self._image_binary: Optional[np.array] = None
+        self._box_analyzer = BoundBoxAnalyzer()
+
+    def update_image(self, image: np.array) -> None:
+        self._image = image
+        self._pre_process_image()
+
+    def _pre_process_image(self) -> None:
+        """
+        Pre process updated image. Convert it to grayscale, threshold it to
+        get binary image and invert it. Image inversion helps with function
+        fill holes, that is looking for white blobs
+        :return: None
+        """
+        gray = self._image_processor.to_gray(self._image)
+        binary = self._image_processor.threshold_image(gray)
+        inverted = self._image_processor.invert(binary)
+
+        self._image_binary = inverted
+
+    @staticmethod
+    def draw_box(image: np.array, rectangle: BoundBox,
+                 color: Tuple[int, int, int] = (0, 255, 0)) -> None:
+        """
+        Draw box on the image with position from BoundBox
+        :param image: image to draw on
+        :param rectangle: BoundBox type, with x start, x end, y start, y end
+        :param color: rgb color to draw rectangle
+        :return: None
+        """
+        start_x, start_y = rectangle.start_x - 1, rectangle.start_y - 1
+        end_x, end_y = rectangle.end_x + 1, rectangle.end_y + 1
+        cv2.rectangle(image, (start_x, start_y), (end_x, end_y), color, 1)
+
+    def draw_boxes(self, image: np.array, boxes: List[BoundBox]) -> None:
+        """
+        Draw boxes on the image
+        :param image:
+        :param boxes: list of BoundBox with pixel positions
+        :return: None
+        """
+        for rectangle in boxes:
+            self.draw_box(image, rectangle)
+
+    def _fill_holes(self) -> None:
+        """
+
+        :return:
+        """
+        contours = self._image_processor.find_contours(self._image_binary)
+
+        return contours
+
+    @staticmethod
+    def _contours_to_boxes(contours: np.array) -> List[BoundBox]:
+        """
+        Create Rectangle boxes around contours
+        :param contours: np.array with 3 el shape
+        :return:
+        """
+        bounds = []
+        for i, contour in enumerate(contours):
+            min_x = min(contour[:, 0, 0])
+            max_x = max(contour[:, 0, 0])
+            min_y = min(contour[:, 0, 1])
+            max_y = max(contour[:, 0, 1])
+            bounds.append(BoundBox(min_x, min_y, max_x, max_y))
+
+        return bounds
+
+    def find_text_boxes(self) -> Tuple[List[List[BoundBox]], List[BoundBox]]:
+        """
+        Use image processing function fill holes, to fill all close shaped
+        contours on the image. This helps reducing amount of contours to analyze
+        Then, find contours on the image. Translate them into list of BoundBox
+        rectangles and analyze them to remove all bounds that are not texts.
+        :return: list of bounds around letters that are within each word,
+        and list of bounds around each word
+        """
+        self._image_processor.fill_holes(self._image_binary)
+        contours = self._image_processor.find_contours(self._image_binary)
+        bound_boxes = self._contours_to_boxes(contours)
+        letters, words = self._box_analyzer.analyze_bound_boxes(bound_boxes)
+
+        return letters, words
---
+++ tests/unittests/recognition/test_text_position_recognition_contours.py
@@ -0,0 +1,105 @@
+from unittest import TestCase
+from unittest.mock import MagicMock, patch, call
+
+import numpy as np
+
+from car_parts_text_recognition.image_processor.image_processor import \
+    ImageProcessor
+from car_parts_text_recognition.recognition.bound_box_analyzer import \
+    BoundBoxAnalyzer
+from car_parts_text_recognition.recognition.text_position_recognition_contours import \
+    TextPositionRecognitionContours
+from car_parts_text_recognition.recognition.utils.bound_box import BoundBox
+
+
+class TestTextPositionRecognitionNoNet(TestCase):
+    def setUp(self) -> None:
+        self.recognition = TextPositionRecognitionContours()
+
+    def test___init__(self):
+        self.assertIsInstance(self.recognition._image_processor,
+                              ImageProcessor)
+        self.assertIsNone(self.recognition._image)
+        self.assertIsNone(self.recognition._image_binary)
+        self.assertIsInstance(self.recognition._box_analyzer,
+                              BoundBoxAnalyzer)
+
+    def test_update_image(self):
+        # given
+        self.recognition._pre_process_image = MagicMock()
+
+        # when
+        self.recognition.update_image('img')
+
+        # then
+        self.assertEqual('img', self.recognition._image)
+        self.recognition._pre_process_image.assert_called()
+
+    def test__pre_process_image(self):
+        # given
+        self.recognition._image_processor = MagicMock()
+        self.recognition._image_processor.invert = MagicMock(return_value='inv')
+
+        # when
+        self.recognition._pre_process_image()
+
+        # then
+        self.recognition._image_processor.to_gray.assert_called()
+        self.recognition._image_processor.threshold_image.assert_called()
+        self.recognition._image_processor.invert.assert_called()
+        self.assertEqual('inv', self.recognition._image_binary)
+
+    @patch('cv2.rectangle')
+    def test_draw_box(self, rectangle_mock):
+        # given
+        rectangle = BoundBox(1, 2, 3, 4)
+        image = MagicMock()
+
+        # when
+        self.recognition.draw_box(image, rectangle)
+
+        # then
+        rectangle_mock.assert_called_with(image, (0, 1), (4, 5), (0, 255, 0), 1)
+
+    def test_draw_boxes(self):
+        # given
+        box1, box2 = MagicMock(), MagicMock()
+        image = MagicMock()
+        self.recognition.draw_box = MagicMock()
+
+        # when
+        self.recognition.draw_boxes(image, [box1, box2])
+
+        # then
+        self.recognition.draw_box.assert_has_calls(
+            [call(image, box1), call(image, box2)])
+
+    def test__contours_to_boxes(self):
+        # given
+        contour1 = np.array([[[1, 4]], [[2, 15]]])
+        contour2 = np.array([[[10, 5]], [[1, 6]], [[3, 25]]])
+
+        # when
+        bounds = self.recognition._contours_to_boxes([contour1, contour2])
+
+        # then
+        self.assertEqual([BoundBox(1, 4, 2, 15), BoundBox(1, 5, 10, 25)],
+                         bounds)
+
+    def test_find_text_boxes(self):
+        # given
+        self.recognition._image_processor = MagicMock()
+        self.recognition._contours_to_boxes = MagicMock()
+        self.recognition._box_analyzer = MagicMock()
+        self.recognition._box_analyzer.analyze_bound_boxes = MagicMock(
+            return_value=('letters', 'words'))
+
+        # when
+        result = self.recognition.find_text_boxes()
+
+        # then
+        self.recognition._image_processor._fill_holes.assert_called()
+        self.recognition._image_processor._find_contours.assert_called()
+        self.recognition._contours_to_boxes.assert_called()
+        self.recognition._box_analyzer.analyze_bound_boxes.assert_called()
+        self.assertEqual(('letters', 'words'), result)
